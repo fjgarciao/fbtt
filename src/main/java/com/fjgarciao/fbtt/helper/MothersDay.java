6a62cfde6abe4050f6fe302aa4d2dc59d909e155
Francisco Javier García Orduña <Francisco.Orduna@king.com>
2017-08-13 20:57:47 +0200

    Finish Mother's Day Marketing Day

--- src/main/java/com/fjgarciao/fbtt/helper/MothersDay.java
+++ src/main/java/com/fjgarciao/fbtt/helper/MothersDay.java
@@ -34,7 +34,6 @@ public class MothersDay implements MarketingDay {
             case AF: // Afghanistan
             case AL: // Albania
             case AZ: // Azerbaijan
-            case BY: // Belarus
             case BA: // Bosnia & Herzegovina
             case BG: // Bulgaria
             case BI: // Burundi
@@ -45,11 +44,9 @@ public class MothersDay implements MarketingDay {
             case MK: // Macedonia
             case MD: // Moldova
             case ME: // Montenegro
-            case RU: // Russia
             case RS: // Serbia
             case TJ: // Tajikistan
             case UZ: // Uzbekistan
-            case VN: // Vietnam
                 c = CalendarUtils.prepareCalendar(year, Calendar.MARCH, 8);
                 break;
             case GG: // Guernsey
@@ -78,11 +75,158 @@ public class MothersDay implements MarketingDay {
             case SA: // Saudi Arabia
             case SO: // Somalia
             case SD: // Sudan
-            case SS: // South Sudan
             case SY: // Syria
             case AE: // United Arab Emirates
             case YE: // Yemen
                 c = CalendarUtils.prepareCalendar(year, Calendar.MARCH, 21);
+                break;
+            case SI: // Slovenia
+                c = CalendarUtils.prepareCalendar(year, Calendar.MARCH, 25);
+                break;
+            case AM: // Armenia
+                c = CalendarUtils.prepareCalendar(year, Calendar.APRIL, 7);
+                break;
+            case AO: // Angola
+            case CV: // Cape Verde
+            case HU: // Hungary
+            case LT: // Lithuania
+            case MZ: // Mozambique
+            case PT: // Portugal
+            case RO: // Romania
+            case ES: // Spain
+                // First Sunday of May
+                c = CalendarUtils.prepareCalendar(year);
+                c.set(Calendar.MONTH, Calendar.MAY);
+                c.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+                c.set(Calendar.DAY_OF_WEEK_IN_MONTH, 1);
+                break;
+            case KR: // South Korea
+                c = CalendarUtils.prepareCalendar(year, Calendar.MAY, 8);
+                break;
+            case SV: // El Salvador
+            case GT: // Guatemala
+            case MX: // Mexico
+                c = CalendarUtils.prepareCalendar(year, Calendar.MAY, 10);
+                break;
+            case AI: // Anguilla
+            case AG: // Antigua & Barbuda
+            case AW: // Aruba
+            case AU: // Australia
+            case AT: // Austria
+            case BS: // Bahamas
+            case BD: // Bangladesh
+            case BB: // Barbados
+            case BE: // Belgium
+            case BZ: // Belize
+            case BM: // Bermuda
+            case BT: // Bhutan
+            case BQ: // Bonaire
+            case BW: // Botswana
+            case BR: // Brazil
+            case BN: // Brunei
+            case CA: // Canada
+            case KH: // Cambodia
+            case KY: // Cayman Islands
+            case CF: // Central African Republic
+            case TD: // Chad
+            case CL: // Chile
+            case CN: // China
+            case CO: // Colombia
+            case CD: // Congo, Dem. Rep.
+            case CG: // Congo, Rep.
+            case CI: // Core d'Ivoire
+            case HR: // Croatia
+            case CU: // Cuba
+            case CW: // Curaçao
+            case CY: // Cyprus
+            case CZ: // Czech Rep.
+            case DK: // Denmark
+            case DM: // Dominica
+            case EC: // Ecuador
+            case GQ: // Equatorial Guinea
+            case EE: // Estonia
+            case ET: // Ethiopia
+            case FO: // Faroe Islands
+            case FJ: // Fiji
+            case FI: // Finland
+            case DE: // Germany
+            case GA: // Gabon
+            case GM: // Gambia
+            case GL: // Greenland
+            case GH: // Ghana
+            case GR: // Greece
+            case GD: // Grenada
+            case GY: // Guyana
+            case HN: // Honduras
+            case HK: // Hong Kong
+            case IS: // Iceland
+            case IN: // India
+            case IT: // Italy
+            case JM: // Jamaica
+            case JP: // Japan
+            case KE: // Kenya
+            case LV: // Latvia
+            case LR: // Liberia
+            case LI: // Liechtenstein
+            case MO: // Macao
+            case MY: // Malaysia
+            case MT: // Malta
+            case MM: // Myanmar
+            case NA: // Namibia
+            case NL: // Netherlands
+            case NZ: // New Zealand
+            case PK: // Pakistan
+            case PG: // Papua New Guinea
+            case PE: // Peru
+            case PH: // Philippines
+            case PR: // Puerto Rico
+            case KN: // Saint Kitts & Nevis
+            case LC: // Saint Lucia
+            case VC: // Saint Vincent & The Grenadines
+            case WS: // Samoa
+            case SG: // Singapore
+            case SX: // Sint Maarten
+            case SK: // Slovakia
+            case ZA: // South Africa
+            case LK: // Sri Lanka
+            case SR: // Suriname
+            case CH: // Switzerland
+            case TW: // Taiwan
+            case TZ: // Tanzania
+            case TO: // Tonga
+            case TT: // Trinidad & Tobago
+            case TR: // Turkey
+            case UG: // Uganda
+            case UA: // Ukraine
+            case US: // United States
+            case UY: // Uruguay
+            case VN: // Vietnam
+            case VE: // Venezuela
+            case ZM: // Zambia
+            case ZW: // Zimbabwe
+                // Second Sunday of May
+                c = CalendarUtils.prepareCalendar(year);
+                c.set(Calendar.MONTH, Calendar.MAY);
+                c.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+                c.set(Calendar.DAY_OF_WEEK_IN_MONTH, 2);
+                break;
+            case BJ: // Benin
+                c = CalendarUtils.prepareCalendar(year, Calendar.MAY, 14);
+                break;
+            case PY: // Paraguay
+                c = CalendarUtils.prepareCalendar(year, Calendar.MAY, 15);
+                break;
+            case KG: // Kyrgyzstan
+                c = CalendarUtils.prepareCalendar(year, Calendar.MAY, 19);
+                break;
+            case IL: // Israel
+                c = CalendarUtils.prepareCalendar(year, Calendar.MAY, 22);
+                break;
+            case PL: // Poland
+                c = CalendarUtils.prepareCalendar(year, Calendar.MAY, 26);
+                break;
+            case BO: // Bolivia
+                c = CalendarUtils.prepareCalendar(year, Calendar.MAY, 27);
                 break;
             case DZ: // Algeria
             case CM: // Cameroon
@@ -92,7 +236,7 @@ public class MothersDay implements MarketingDay {
             case MA: // Morocco
             case NE: // Niger
             case HT: // Haiti
-            case MU: // Mautitius
+            case MU: // Mauritius
             case SN: // Senegal
             case SE: // Sweden
             case TN: // Tunisia
@@ -120,25 +264,99 @@ public class MothersDay implements MarketingDay {
                     c.add(Calendar.WEEK_OF_YEAR, 1);
                 }
                 break;
-            case SI: // Slovenia
-                c = CalendarUtils.prepareCalendar(year, Calendar.MARCH, 25);
+            case NI: // Nicaragua
+                c = CalendarUtils.prepareCalendar(year, Calendar.MAY, 30);
                 break;
-            case AM: // Armenia
-                c = CalendarUtils.prepareCalendar(year, Calendar.APRIL, 7);
+            case MN: // Mongolia
+                c = CalendarUtils.prepareCalendar(year, Calendar.JUNE, 1);
                 break;
-            case AO: // Angola
-            case CV: // Cape Verde
-            case HU: // Hungary
-            case LT: // Lithuania
-            case MZ: // Mozambique
-            case PT: // Portugal
-            case RO: // Romania
-            case ES: // Spain
-                // First Sunday of May
+            case LU: // Luxembourg
+                // Second Sunday of June
                 c = CalendarUtils.prepareCalendar(year);
-                c.set(Calendar.MONTH, Calendar.MAY);
+                c.set(Calendar.MONTH, Calendar.JUNE);
                 c.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+                c.set(Calendar.DAY_OF_WEEK_IN_MONTH, 2);
+                break;
+            case SS: // South Sudan
+                // First Monday of July
+                c = CalendarUtils.prepareCalendar(year);
+                c.set(Calendar.MONTH, Calendar.JULY);
+                c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
                 c.set(Calendar.DAY_OF_WEEK_IN_MONTH, 1);
+                break;
+            case TH: // Thailand
+                c = CalendarUtils.prepareCalendar(year, Calendar.AUGUST, 12);
+                break;
+            case CR: // Costa Rica
+                c = CalendarUtils.prepareCalendar(year, Calendar.AUGUST, 15);
+                break;
+            case MW: // Malawi
+                // Second Monday of October
+                c = CalendarUtils.prepareCalendar(year);
+                c.set(Calendar.MONTH, Calendar.OCTOBER);
+                c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+                c.set(Calendar.DAY_OF_WEEK_IN_MONTH, 2);
+                break;
+            case BY: // Belarus
+                c = CalendarUtils.prepareCalendar(year, Calendar.OCTOBER, 14);
+                break;
+            case AR: // Argentina
+                // Third Sunday of October
+                c = CalendarUtils.prepareCalendar(year);
+                c.set(Calendar.MONTH, Calendar.OCTOBER);
+                c.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+                c.set(Calendar.DAY_OF_WEEK_IN_MONTH, 3);
+                break;
+            case TL: // Timor-Leste
+                c = CalendarUtils.prepareCalendar(year, Calendar.NOVEMBER, 3);
+                break;
+            case KP: // North Korea
+                c = CalendarUtils.prepareCalendar(year, Calendar.NOVEMBER, 16);
+                break;
+            case RU: // Russia
+                // Last Sunday of November
+                c = CalendarUtils.prepareCalendar(year);
+                c.set(Calendar.MONTH, Calendar.NOVEMBER);
+                c.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+                c.set(Calendar.DAY_OF_WEEK_IN_MONTH, 4);
+                if (c.get(Calendar.DAY_OF_MONTH) <= 23)
+                    c.set(Calendar.DAY_OF_WEEK_IN_MONTH, 5);
+                break;
+            case PA: // Panama
+                c = CalendarUtils.prepareCalendar(year, Calendar.DECEMBER, 8);
+                break;
+            case ID: // Indonesia
+                c = CalendarUtils.prepareCalendar(year, Calendar.DECEMBER, 22);
+                break;
+            case NP: // Nepal
+                switch(year) {
+                    case 2017:
+                        c = CalendarUtils.prepareCalendar(year, Calendar.APRIL, 26);
+                        break;
+                    case 2018:
+                        c = CalendarUtils.prepareCalendar(year, Calendar.APRIL, 21);
+                        break;
+                }
+                break;
+            case IR: // Iran
+                switch(year) {
+                    case 2017:
+                        c = CalendarUtils.prepareCalendar(year, Calendar.FEBRUARY, 28);
+                        break;
+                    case 2018:
+                        c = CalendarUtils.prepareCalendar(year, Calendar.FEBRUARY, 17);
+                        break;
+                    case 2019:
+                        c = CalendarUtils.prepareCalendar(year, Calendar.FEBRUARY, 6);
+                        break;
+                    case 2020:
+                        c = CalendarUtils.prepareCalendar(year, Calendar.JANUARY, 26);
+                        break;
+                    case 2021:
+                        c = CalendarUtils.prepareCalendar(year, Calendar.JANUARY, 14);
+                        break;
+                }
+                c.add(Calendar.DATE, 20);
                 break;
         }
         return c == null ? Optional.empty() : Optional.of(c.getTime());
--- src/test/java/com/fjgarciao/fbtt/helper/MothersDayTest.java
+++ src/test/java/com/fjgarciao/fbtt/helper/MothersDayTest.java
@@ -22,55 +22,122 @@ public class MothersDayTest {
 
     @Test
     public void testNorway() {
-        Calendar no2017 = CalendarUtils.prepareCalendar(2017, Calendar.FEBRUARY, 12);
-        Calendar no2018 = CalendarUtils.prepareCalendar(2018, Calendar.FEBRUARY, 11);
+        Calendar d2017 = CalendarUtils.prepareCalendar(2017, Calendar.FEBRUARY, 12);
+        Calendar d2018 = CalendarUtils.prepareCalendar(2018, Calendar.FEBRUARY, 11);
 
-        Assert.assertThat(uit.getDate(CountryCode.NO, 2017).get(), is(equalTo(no2017.getTime())));
-        Assert.assertThat(uit.getDate(CountryCode.NO, 2018).get(), is(equalTo(no2018.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.NO, 2017).get(), is(equalTo(d2017.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.NO, 2018).get(), is(equalTo(d2018.getTime())));
     }
 
     @Test
     public void testMotheringSunday() {
-        Calendar ms2017 = CalendarUtils.prepareCalendar(2017, Calendar.MARCH, 26);
-        Calendar ms2018 = CalendarUtils.prepareCalendar(2018, Calendar.MARCH, 11);
-        Calendar ms2019 = CalendarUtils.prepareCalendar(2019, Calendar.MARCH, 31);
+        Calendar d2017 = CalendarUtils.prepareCalendar(2017, Calendar.MARCH, 26);
+        Calendar d2018 = CalendarUtils.prepareCalendar(2018, Calendar.MARCH, 11);
+        Calendar d2019 = CalendarUtils.prepareCalendar(2019, Calendar.MARCH, 31);
 
-        Assert.assertThat(uit.getDate(CountryCode.UK, 2017).get(), is(equalTo(ms2017.getTime())));
-        Assert.assertThat(uit.getDate(CountryCode.UK, 2018).get(), is(equalTo(ms2018.getTime())));
-        Assert.assertThat(uit.getDate(CountryCode.UK, 2019).get(), is(equalTo(ms2019.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.UK, 2017).get(), is(equalTo(d2017.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.UK, 2018).get(), is(equalTo(d2018.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.UK, 2019).get(), is(equalTo(d2019.getTime())));
     }
 
     @Test
     public void testSpain() {
-        Calendar es2017 = CalendarUtils.prepareCalendar(2017, Calendar.MAY, 7);
-        Calendar es2018 = CalendarUtils.prepareCalendar(2018, Calendar.MAY, 6);
+        Calendar d2017 = CalendarUtils.prepareCalendar(2017, Calendar.MAY, 7);
+        Calendar d2018 = CalendarUtils.prepareCalendar(2018, Calendar.MAY, 6);
 
-        Assert.assertThat(uit.getDate(CountryCode.ES, 2017).get(), is(equalTo(es2017.getTime())));
-        Assert.assertThat(uit.getDate(CountryCode.ES, 2018).get(), is(equalTo(es2018.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.ES, 2017).get(), is(equalTo(d2017.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.ES, 2018).get(), is(equalTo(d2018.getTime())));
     }
 
     @Test
     public void testLastSundayOfMay() {
-        Calendar lsm2017 = CalendarUtils.prepareCalendar(2017, Calendar.MAY, 28);
-        Calendar lsm2018 = CalendarUtils.prepareCalendar(2018, Calendar.MAY, 27);
-        Calendar lsm2020 = CalendarUtils.prepareCalendar(2020, Calendar.MAY, 31);
-        Calendar lsm2021 = CalendarUtils.prepareCalendar(2021, Calendar.MAY, 30);
-        Calendar lsm2022 = CalendarUtils.prepareCalendar(2022, Calendar.MAY, 29);
+        Calendar d2017 = CalendarUtils.prepareCalendar(2017, Calendar.MAY, 28);
+        Calendar d2018 = CalendarUtils.prepareCalendar(2018, Calendar.MAY, 27);
+        Calendar d2020 = CalendarUtils.prepareCalendar(2020, Calendar.MAY, 31);
+        Calendar d2021 = CalendarUtils.prepareCalendar(2021, Calendar.MAY, 30);
+        Calendar d2022 = CalendarUtils.prepareCalendar(2022, Calendar.MAY, 29);
 
-        Assert.assertThat(uit.getDate(CountryCode.SE, 2017).get(), is(equalTo(lsm2017.getTime())));
-        Assert.assertThat(uit.getDate(CountryCode.SE, 2018).get(), is(equalTo(lsm2018.getTime())));
-        Assert.assertThat(uit.getDate(CountryCode.SE, 2020).get(), is(equalTo(lsm2020.getTime())));
-        Assert.assertThat(uit.getDate(CountryCode.SE, 2021).get(), is(equalTo(lsm2021.getTime())));
-        Assert.assertThat(uit.getDate(CountryCode.SE, 2022).get(), is(equalTo(lsm2022.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.SE, 2017).get(), is(equalTo(d2017.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.SE, 2018).get(), is(equalTo(d2018.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.SE, 2020).get(), is(equalTo(d2020.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.SE, 2021).get(), is(equalTo(d2021.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.SE, 2022).get(), is(equalTo(d2022.getTime())));
     }
     
     @Test
     public void testFrance() {
-        Calendar fr2019 = CalendarUtils.prepareCalendar(2019, Calendar.MAY, 26);
-        Calendar fr2020 = CalendarUtils.prepareCalendar(2020, Calendar.JUNE, 7);
+        Calendar d2019 = CalendarUtils.prepareCalendar(2019, Calendar.MAY, 26);
+        Calendar d2020 = CalendarUtils.prepareCalendar(2020, Calendar.JUNE, 7);
 
-        Assert.assertThat(uit.getDate(CountryCode.FR, 2019).get(), is(equalTo(fr2019.getTime())));
-        Assert.assertThat(uit.getDate(CountryCode.FR, 2020).get(), is(equalTo(fr2020.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.FR, 2019).get(), is(equalTo(d2019.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.FR, 2020).get(), is(equalTo(d2020.getTime())));
     }
 
+    @Test
+    public void testLuxembourg() {
+        Calendar d2017 = CalendarUtils.prepareCalendar(2017, Calendar.JUNE, 11);
+        Calendar d2018 = CalendarUtils.prepareCalendar(2018, Calendar.JUNE, 10);
+
+        Assert.assertThat(uit.getDate(CountryCode.LU, 2017).get(), is(equalTo(d2017.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.LU, 2018).get(), is(equalTo(d2018.getTime())));
+    }
+
+    @Test
+    public void testSouthSudan() {
+        Calendar d2017 = CalendarUtils.prepareCalendar(2017, Calendar.JULY, 3);
+        Calendar d2018 = CalendarUtils.prepareCalendar(2018, Calendar.JULY, 2);
+
+        Assert.assertThat(uit.getDate(CountryCode.SS, 2017).get(), is(equalTo(d2017.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.SS, 2018).get(), is(equalTo(d2018.getTime())));
+    }
+
+    @Test
+    public void testMalawi() {
+        Calendar d2017 = CalendarUtils.prepareCalendar(2017, Calendar.OCTOBER, 9);
+        Calendar d2018 = CalendarUtils.prepareCalendar(2018, Calendar.OCTOBER, 8);
+
+        Assert.assertThat(uit.getDate(CountryCode.MW, 2017).get(), is(equalTo(d2017.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.MW, 2018).get(), is(equalTo(d2018.getTime())));
+    }
+    
+    @Test
+    public void testArgentina() {
+        Calendar d2017 = CalendarUtils.prepareCalendar(2017, Calendar.OCTOBER, 15);
+        Calendar d2018 = CalendarUtils.prepareCalendar(2018, Calendar.OCTOBER, 21);
+
+        Assert.assertThat(uit.getDate(CountryCode.AR, 2017).get(), is(equalTo(d2017.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.AR, 2018).get(), is(equalTo(d2018.getTime())));
+    }
+
+    @Test
+    public void testLastSundayOfNovember() {
+        Calendar d2017 = CalendarUtils.prepareCalendar(2017, Calendar.NOVEMBER, 26);
+        Calendar d2018 = CalendarUtils.prepareCalendar(2018, Calendar.NOVEMBER, 25);
+        Calendar d2019 = CalendarUtils.prepareCalendar(2019, Calendar.NOVEMBER, 24);
+        Calendar d2020 = CalendarUtils.prepareCalendar(2020, Calendar.NOVEMBER, 29);
+        Calendar d2021 = CalendarUtils.prepareCalendar(2021, Calendar.NOVEMBER, 28);
+        Calendar d2022 = CalendarUtils.prepareCalendar(2022, Calendar.NOVEMBER, 27);
+
+        Assert.assertThat(uit.getDate(CountryCode.RU, 2017).get(), is(equalTo(d2017.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.RU, 2018).get(), is(equalTo(d2018.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.RU, 2019).get(), is(equalTo(d2019.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.RU, 2020).get(), is(equalTo(d2020.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.RU, 2021).get(), is(equalTo(d2021.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.RU, 2022).get(), is(equalTo(d2022.getTime())));
+    }
+
+    @Test
+    public void testIran() {
+        Calendar d2017 = CalendarUtils.prepareCalendar(2017, Calendar.MARCH, 20);
+        Calendar d2018 = CalendarUtils.prepareCalendar(2018, Calendar.MARCH, 9);
+        Calendar d2019 = CalendarUtils.prepareCalendar(2019, Calendar.FEBRUARY, 26);
+        Calendar d2020 = CalendarUtils.prepareCalendar(2020, Calendar.FEBRUARY, 15);
+        Calendar d2021 = CalendarUtils.prepareCalendar(2021, Calendar.FEBRUARY, 3);
+
+        Assert.assertThat(uit.getDate(CountryCode.IR, 2017).get(), is(equalTo(d2017.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.IR, 2018).get(), is(equalTo(d2018.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.IR, 2019).get(), is(equalTo(d2019.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.IR, 2020).get(), is(equalTo(d2020.getTime())));
+        Assert.assertThat(uit.getDate(CountryCode.IR, 2021).get(), is(equalTo(d2021.getTime())));
+    }
 }
